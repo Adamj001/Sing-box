765f89c40894c7fd4e89a5d7f9eee4492a8e267f
Adamj001 <130832350+Adamj001@users.noreply.github.com>
2024-08-14 09:37:35 +0800

    sb_serv00.sh

--- sb_serv00.sh
+++ sb_serv00.sh
@@ -14,7 +14,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'5cb79487-9a92-4b2f-aa6d-d7aff1c0a256'}
+export UUID=${UUID:-'56b02a41-0259-474d-80d4-9021d81f55b2'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
